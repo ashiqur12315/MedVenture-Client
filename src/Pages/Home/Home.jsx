22ce6ea71bdd2db30f676e8e69517575ad8ed579
ashiqur12315 <ashiqur12315@gmial.com>
2024-06-22 20:05:58 +0600

    Added website logo and succeess message for public subsciption

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -20,6 +20,7 @@ const Home = () => {
                 <SectionTitle heading='Popular Medical Camps' subheading='Join us at our Popular medical camps to receive expert healthcare services, tailored to meet the needs of every individual.'></SectionTitle>
                 <PopularCamps></PopularCamps>
                 <div className="container mx-auto text-center"><Link className="m-6 btn mx-auto btn-info" to='/availableCamps'>See All Camps</Link></div>
+                <SectionTitle heading='Our Success Stories' subheading='Happiness is Our Goal'></SectionTitle>
                 <div><HomeFeedback></HomeFeedback></div>
             </div>
             <Sub></Sub>
