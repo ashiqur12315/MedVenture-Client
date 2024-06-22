d0a56c99b518bca1f4dec5cc88ecbf90c57b81e1
ashiqur12315 <ashiqur12315@gmial.com>
2024-06-22 19:59:39 +0600

    Added contact route for storing the public user message

--- src/Pages/Home/Subscribe.jsx
+++ src/Pages/Home/Subscribe.jsx
@@ -13,22 +13,24 @@ const Subscribe = () => {
         const sub = {
             name, email, message
         }
+        console.log(sub)
 
         axiosPublic.post('/sub', sub)
         .then(res =>{
             if(res.data.insertedId){
                 Swal.fire({
-                    title: "Deleted!",
-                    text: "Your file has been deleted.",
+                    title: "Subscribed!",
+                    text: "You will be contacted soon , Thank You",
                     icon: "success"
                   });
+                  e.currentTarget.reset();
             }
         })
 
 
     }
     return (
-        <section className="py-6 dark:bg-gray-100 dark:text-gray-900">
+        <section className="py-6 dark:bg-gray-100 dark:text-gray-900 ">
             <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
                 <div className="py-6 md:py-0 md:px-6">
                     <h1 className="text-4xl font-bold">Get in touch</h1>
@@ -68,7 +70,7 @@ const Subscribe = () => {
                         <span className="mb-1">Message</span>
                         <textarea rows="3" name="message" placeholder="Enter your message here..." className="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:dark:ring-red-600 dark:bg-gray-100"></textarea>
                     </label>
-                    <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 dark:bg-red-600 dark:text-gray-50 focus:dark:ring-red-600 hover:dark:ring-red-600">Submit</button>
+                    <button type="submit" className="btn btn-outline btn-success self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 dark:bg-red-600 dark:text-gray-50 focus:dark:ring-red-600 hover:dark:ring-red-600">Submit</button>
                 </form>
             </div>
         </section>
