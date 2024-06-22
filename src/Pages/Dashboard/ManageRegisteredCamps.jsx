49ff74ec3b857392fba035032998dbcf75e2391d
ashiqur12315 <ashiqur12315@gmial.com>
2024-06-22 20:59:01 +0600

    Implemented search bar on manage Registered camps route in admin dashboard

--- src/Pages/Dashboard/ManageRegisteredCamps.jsx
+++ src/Pages/Dashboard/ManageRegisteredCamps.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 import SectionTitle from "../../Components/Shared/SectionTitle";
+import { useState } from "react";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 
 const ManageRegisteredCamps = () => {
@@ -50,9 +52,50 @@ const ManageRegisteredCamps = () => {
             }
         });
     }
+
+    const [search, setSearch] = useState('')
+    const axiosPublic = useAxiosPublic()
+
+    const { data: searchCamp = [], isLoading: searchCampLoading, refetch: reload } = useQuery({
+        queryFn: () => getData(),
+        queryKey: ['searchCamp', search],
+        // enabled: false
+    })
+    const getData = async () => {
+        // const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/search-job?search=${search}`)
+        const { data } = await axiosPublic.get(`/search-camp-reg?search=${search}`)
+        return data
+    }
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget)
+        const search = form.get('search')
+        setSearch(search)
+        await reload();
+    };
+    console.log(searchCamp)
+
     return (
         <div>
             <SectionTitle heading='Manage All Users Registered Camps'></SectionTitle>
+            <div>
+                <form onSubmit={handleSearch}>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="border border-gray-300 rounded-l px-4 py-2 focus:outline-none focus:ring focus:border-blue-400"
+                        name="search"
+
+                    />
+                    <button type="submit"
+                        className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                    // onClick={handleSearch}
+                    >
+                        Search
+                    </button>
+                </form>
+            </div>
             {
                 loading ? <span className="loading loading-spinner text-info"></span>
                     :
@@ -71,37 +114,83 @@ const ManageRegisteredCamps = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {loading ? <span className="loading loading-spinner text-info"></span> :
-                                        allParticipants.map((p, index) => <tr key={p._id}>
-                                            <th>{p.participant_name}</th>
-                                            <td>{p.name}</td>
-                                            <td>{p.fees}</td>
-                                            <td>{p.payment_status ? p.payment_status : 'Unpaid'}</td>
+                                    {
+                                        search.length > 0 ?
+                                            <>
 
-                                            <td>
-                                                {
-                                                    p.payment_status ? <>
-                                                        {p.confirmation_status ? p.confirmation_status :
+                                                {searchCampLoading ? <span className="loading loading-spinner text-info"></span> :
+                                                    searchCamp.map((p, index) => <tr key={p._id}>
+                                                        <th>{p.participant_name}</th>
+                                                        <td>{p.name}</td>
+                                                        <td>{p.fees}</td>
+                                                        <td>{p.payment_status ? p.payment_status : 'Unpaid'}</td>
 
-                                                            <button onClick={() => handleConfirmation(p._id)} className="btn  btn-success">Pending</button>
+                                                        <td>
+                                                            {
+                                                                p.payment_status ? <>
+                                                                    {p.confirmation_status ? p.confirmation_status :
 
-                                                        }
-                                                    </> : 'pending'
+                                                                        <button onClick={() => handleConfirmation(p._id)} className="btn  btn-success">Pending</button>
+
+                                                                    }
+                                                                </> : 'pending'
+                                                            }
+
+                                                        </td>
+                                                        <td>
+                                                            {
+                                                                (p.payment_status && p.confirmation_status) ?
+                                                                    <button disabled className="btn btn-warning">Cancel</button>
+                                                                    :
+                                                                    <button onClick={() => handleCancel(p._id)} className="btn btn-warning">Cancel</button>
+
+                                                            }
+                                                        </td>
+
+                                                    </tr>)
                                                 }
 
-                                            </td>
-                                            <td>
-                                                {
-                                                    (p.payment_status && p.confirmation_status)? 
-                                                    <button disabled className="btn btn-warning">Cancel</button>
-                                                    :
-                                                    <button onClick={()=> handleCancel(p._id)} className="btn btn-warning">Cancel</button>
-                                                    
-                                                }
-                                            </td>
+                                            </>
+                                            :
+                                            <>
+                                                {loading ? <span className="loading loading-spinner text-info"></span> :
+                                                    allParticipants.map((p, index) => <tr key={p._id}>
+                                                        <th>{p.participant_name}</th>
+                                                        <td>{p.name}</td>
+                                                        <td>{p.fees}</td>
+                                                        <td>{p.payment_status ? p.payment_status : 'Unpaid'}</td>
 
-                                        </tr>)
+                                                        <td>
+                                                            {
+                                                                p.payment_status ? <>
+                                                                    {p.confirmation_status ? p.confirmation_status :
+
+                                                                        <button onClick={() => handleConfirmation(p._id)} className="btn  btn-success">Pending</button>
+
+                                                                    }
+                                                                </> : 'pending'
+                                                            }
+
+                                                        </td>
+                                                        <td>
+                                                            {
+                                                                (p.payment_status && p.confirmation_status) ?
+                                                                    <button disabled className="btn btn-warning">Cancel</button>
+                                                                    :
+                                                                    <button onClick={() => handleCancel(p._id)} className="btn btn-warning">Cancel</button>
+
+                                                            }
+                                                        </td>
+
+                                                    </tr>)
+                                                }
+                                            </>
                                     }
+
+
+
+
+
 
 
                                 </tbody>
